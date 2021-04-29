460cc25bfd388555f01969ef532d194439326a24
Jython1415 <36921818+Jython1415@users.noreply.github.com>
2021-04-29 17:25:51 +0900

    debugging code

--- Genshin_forever/Genshin_forever/src/main.cpp
+++ Genshin_forever/Genshin_forever/src/main.cpp
@@ -8,10 +8,22 @@ using namespace vex;
 competition Competition;
 
 void user_control() {
+  int graph[255];
+
+  for (int i = 0; i < 255; i++) {
+    graph[i] = c_equation(i - 127);
+  }
+
+  for (int i = 0; i < 255; i++) {
+    std::cout << (i - 127) << " " << graph[i] << std::endl;
+  }
+
   while (true) {
     tank_control();
     intake_control();
     slow_mode_();
+
+    std::cout << A3 << std::endl;
   }
 }
 
