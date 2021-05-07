b9ff56d78b5eceaffa62a4b6ef8bcaf9b9b48f9d
Jython1415 <36921818+Jython1415@users.noreply.github.com>
2021-05-07 16:01:57 +0900

    remove old code

--- Genshin_forever/Genshin_forever/include/driver_control.h
+++ Genshin_forever/Genshin_forever/include/driver_control.h
@@ -4,17 +4,6 @@
 #include "vex.h"
 #include "pragma.h"
 
-// curve mapping constants
-extern bool curve_is_on; // on and off for curved controller mapping
-extern double c_exponent;
-extern int c_lower_dz; // lower dead zone; dead zone being the part of the joystick range that has no effect
-extern int c_upper_dz; // upper dead zone
-extern int c_lowest_pwr; // lowest power output
-// derived using desmos.com/calculator/rzxfys8ctn -- a calculator I developed for this purpose
-extern double c_multiplier;
-int c_equation (int axis_input);
-void c_mapping_initialize();
-
 extern float A1;
 extern float A2;
 extern float A3;
--- Genshin_forever/Genshin_forever/src/driver_control.cpp
+++ Genshin_forever/Genshin_forever/src/driver_control.cpp
@@ -1,24 +1,7 @@
 #include "driver_control.h"
+#include "controller-mapping.cpp"
 
 controller ctrl;
-
-// curve mapping constants
-bool curve_is_on = 1; // on and off for curved controller mapping
-double c_exponent = 2.0;
-int c_lower_dz = 10; // lower dead zone; dead zone being the part of the joystick range that has no effect
-int c_upper_dz = 10; // upper dead zone
-int c_lowest_pwr = 3; // lowest power output
-// derived using desmos.com/calculator/rzxfys8ctn -- a calculator I developed for this purpose
-double c_multiplier = 0;
-// math for the mapping
-int c_equation (int axis_input)
-{
-  if (abs(axis_input) >= 127 - c_upper_dz) { return copysign(100, axis_input); }
-  else if (abs(axis_input) <= c_lower_dz) { return 0; }
-  else { return int(copysign(round(c_multiplier * pow(abs(axis_input) - c_lower_dz, c_exponent)) + c_lowest_pwr, axis_input)); }
-}
-// initiaizing the mapping constant
-void c_mapping_initialize() { c_multiplier = (100 - c_lowest_pwr) / pow(127 - (c_upper_dz + c_lower_dz), c_exponent); }
 
 float slow_mode = 1;
 
@@ -38,10 +21,10 @@ void spin (motor name, int power) {
 }
 
 void tank_control() {
-  spin(leftfront, c_equation(A3) * slow_mode);
-  spin(leftback, c_equation(A3) * slow_mode);
-  spin(rightfront, c_equation(A2) * slow_mode);
-  spin(rightback, c_equation(A2) * slow_mode);
+  spin(leftfront, c_map(A3) * slow_mode);
+  spin(leftback, c_map(A3) * slow_mode);
+  spin(rightfront, c_map(A2) * slow_mode);
+  spin(rightback, c_map(A2) * slow_mode);
 }
 
 void intake_v_1() {
